df9dde0dabca0bbf0fbf631ebf1c6e8d8e554d6c
ayushacharya2010 <ayushbibo@gmail.com>
2024-07-27 16:56:02 -0500

    Create functioning calculator

--- script.js
+++ script.js
@@ -31,3 +31,112 @@ IF ANY NUMBER, WHETHER INPUT OR OUTPUT, IS
     AND CLEAR EVERYTHING
 
 */
+
+let currentNumber = "";
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+let result = null;
+
+function addNums(firstNum, secondNum) {
+    return firstNum + secondNum;
+};
+
+function subtractNums(firstNum, secondNum) {
+    return firstNum - secondNum;
+};
+
+function multiplyNums(firstNum, secondNum) {
+    return firstNum * secondNum;
+};
+
+function divideNums(firstNum, secondNum) {
+    return firstNum / secondNum;
+};
+
+const numberButtonNodeList = document.querySelectorAll("button.number");
+
+const numberButtonArray = [...numberButtonNodeList];
+
+numberButtonArray.forEach(button => {
+    button.addEventListener("click", () => {
+        updateCurrentNumber(button.textContent);
+    });
+});
+
+function updateCurrentNumber(number) {
+    currentNumber += number;
+    let outputText = document.querySelector("h1.output-text");
+    outputText.textContent = currentNumber;
+    return currentNumber;
+}
+
+const operatorButtonNodeList = document.querySelectorAll("button.operator");
+
+const operatorButtonArray = [...operatorButtonNodeList];
+
+operatorButtonArray.forEach(button => {
+    button.addEventListener("click", () => {
+        if (operator === null) {
+            operator = button.textContent;
+            let outputText = document.querySelector("h1.output-text");
+            firstNum = parseFloat(outputText.textContent);
+            currentNumber = "";
+            outputText.textContent = "";   
+        } else {
+            calculate();
+        }
+    });
+});
+
+const equalButton = document.querySelector("button.equals");
+
+equalButton.addEventListener("click", () => {
+    calculate();
+});
+
+function calculate() {
+    if (currentNumber === null) {
+        return;
+    } else {
+        secondNum = parseFloat(currentNumber);
+        switch (operator) {
+            case '+':
+                result = addNums(firstNum, secondNum);
+                break;
+            case '-':
+                result = subtractNums(firstNum, secondNum);
+                break;
+            case '*':
+                result = multiplyNums(firstNum, secondNum);
+                break;
+            case '/':
+                if (secondNum === 0) {
+                    result = "Idiot";
+                } else {
+                    result = divideNums(firstNum, secondNum);
+                }
+                break;
+        }
+        let outputText = document.querySelector("h1.output-text");
+        outputText.textContent = result;
+    
+        currentNumber = "";
+        firstNum = result;
+        secondNum = null;
+        operator = null;    
+    }
+}
+
+const clearButton = document.querySelector("button.clear");
+
+clearButton.addEventListener("click", () => {
+    currentNumber = "";
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+    result = null;
+
+    let outputText = document.querySelector("h1.output-text");
+    outputText.textContent = currentNumber;
+});
